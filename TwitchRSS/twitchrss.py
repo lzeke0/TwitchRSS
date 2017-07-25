8944c521f78f6551b9adc0a98ee9b8d913cd666d
Laszlo Zeke <kkocii@gmail.com>
2017-07-26 00:59:52 +0200

    Increase vod data cache lifetime to 10 mins

--- TwitchRSS/twitchrss.py
+++ TwitchRSS/twitchrss.py
@@ -29,7 +29,7 @@ VODCACHE_PREFIX = 'vodcache'
 USERIDCACHE_PREFIX = 'userid'
 VOD_URL_TEMPLATE = 'https://api.twitch.tv/kraken/channels/%s/videos?broadcast_type=archive,highlight,upload&limit=5'
 USERID_URL_TEMPLATE = 'https://api.twitch.tv/kraken/users?login=%s'
-VODCACHE_LIFETIME = 120
+VODCACHE_LIFETIME = 600
 USERIDCACHE_LIFETIME = 0  # No expire
 
 
